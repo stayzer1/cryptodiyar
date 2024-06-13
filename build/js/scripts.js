f39e5ba65925a0a13cb73ef0cd22803c8183940a
stayzer <stayzer2@gmail.com>
2024-06-13 20:38:54 +0300

    first upload

---
+++ build/js/scripts.js
@@ -0,0 +1,95 @@
+// Custom Scripts
+// Custom scripts
+
+// Smooth Scroll
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+  smoothLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = smoothLink.getAttribute("href");
+
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 3,
+  enabled: true,
+  breakpoints: {
+    360: {
+      grabCursor: true,
+      slidesPerView: "auto",
+      freeMode: true,
+      centeredSlides: true,
+      loop: false,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1200: {
+      slidesPerView: 3,
+      loop: true,
+      spaceBetween: 40,
+      // centeredSlides: true,
+      watchOverflow: true,
+    },
+  },
+});
+
+// Мобильное меню бургер
+function burgerMenu() {
+  const burger = document.querySelector(".burger");
+  const menu = document.querySelector(".menu");
+  const body = document.querySelector("body");
+  burger.addEventListener("click", () => {
+    if (!menu.classList.contains("active")) {
+      menu.classList.add("active");
+      burger.classList.add("active-burger");
+      body.classList.add("locked");
+    } else {
+      menu.classList.remove("active");
+      burger.classList.remove("active-burger");
+      body.classList.remove("locked");
+    }
+  });
+  // Вот тут мы ставим брейкпоинт навбара
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 991.98) {
+      menu.classList.remove("active");
+      burger.classList.remove("active-burger");
+      body.classList.remove("locked");
+    }
+  });
+}
+burgerMenu();
+
+// Аккордеон
+function accordion() {
+  const items = document.querySelectorAll(".accordion__item-trigger");
+  items.forEach((item) => {
+    item.addEventListener("click", () => {
+      const parent = item.parentNode;
+      if (parent.classList.contains("accordion__item-active")) {
+        parent.classList.remove("accordion__item-active");
+      } else {
+        document
+          .querySelectorAll(".accordion__item")
+          .forEach((child) => child.classList.remove("accordion__item-active"));
+        parent.classList.add("accordion__item-active");
+      }
+    });
+  });
+}
+accordion();
+
